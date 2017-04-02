c6e5e2725d52e224a0e5c0616edd30667fee9ca6
kohbo <kohbosan@gmail.com>
2017-04-02 04:28:18 -0400

    Added methods to injectData into ARView

--- app/src/main/java/com/hertz/jalapeno/beacon/ARActivity.java
+++ app/src/main/java/com/hertz/jalapeno/beacon/ARActivity.java
@@ -19,17 +19,15 @@ import com.wikitude.architect.ArchitectView;
 
 import java.io.IOException;
 
-/**
- * Created by kohbo on 4/2/2017.
- */
-
-public abstract class ARActivity extends AppCompatActivity {
+public abstract class ARActivity extends AppCompatActivity implements ArchitectView.ArchitectWorldLoadedListener{
     private final String TAG = "ARActivity";
     private final int GEO_PERM_ID = 1234;
     private final int CAMERA_PERM_ID = 8008;
 
     private LocationManager locationManager;
     private ArchitectView architectView;
+
+    protected boolean isLoading = false;
 
     private String ARkey() {
         String key = "pPRPYEu94bqvCrDihHUq42Dob1CNySsICF9gGbnEmxah3jGTy7l1UctEkyj0CFS7KgrHKSYq9iihedyj7dEjKVObozE01QQlbOh1bPklkN5huNylkNS2Ec0NGzYP8fmWJXkfSytsE628OMIJe6sr+XAS6pRbtVaDn2OloyWD1wFTYWx0ZWRfX40YS1TiJtIPOmVv7e1qa1MeNyARYukrmEifZjIkY9GDggJ+jocqxrZjnnwIyrKVbFEF+Eg97XLbT0e4VT45V/NEjRJTQAsCq1YvpCk2N/Z1znmVZM/VHwF7uBU6/RgFkRq7cUGnMImPlQ5fACMzCYlpxQm/XTywLPDrzDTVzkKw+dBSnTeWB1ZAdfeFC/YttlLxOfwQqZmEyYS64DNHzYvsw6GyMwCFPa6RZ8fObcn+a3bk5JcBtqSLJPgjJ0MxiZiWRg2fSOiyR42/uhy/MpvD1+BYpUkGR/cryK5KKSzffMBlLHGwevKlhO2L5//+M+qG57AavXNIPjhSFe9qhy1WURl6Nj274xUL0C3hGQ4zd/7GMl59CxboiJuZt3equpreeWxCHtBym61Hsz9dxr1hQtoXd7xq40KN/zHMSk7/hVMeFXg5Co3cn7YmA7G6P5YoedzeTqb8RiSEEkuj9p0gScPXZGT/XWbj6yiIr2GAeYmEODf4tdo=";
@@ -40,9 +38,10 @@ public abstract class ARActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         architectView.onPostCreate();
+        architectView.registerWorldLoadedListener(this);
+
         try {
             architectView.load(getAssetPath());
-            architectView.setLocation(26.419498, -81.810104, 10.0);
         } catch (IOException e) {
             Log.d(TAG, "Unable to load html file.");
         }
@@ -145,5 +144,83 @@ public abstract class ARActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void worldWasLoaded(String s) {
+        Log.d(TAG, "World was loaded.");
+        architectView.setLocation(26.419498, -81.810104, 20.0);
+    }
+
+    @Override
+    public void worldLoadFailed(int i, String s, String s1) {
+        Log.d(TAG, "World load failed.");
+    }
+
+    protected void injectData() {
+        if (!isLoading) {
+            final Thread t = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    isLoading = true;
+
+                    final int WAIT_FOR_LOCATION_STEP_MS = 2000;
+
+                    while (!isFinishing()) {
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Toast.makeText(ARActivity.this, R.string.location_fetching, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        try {
+                            Thread.sleep(WAIT_FOR_LOCATION_STEP_MS);
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                    }
+
+                    if (!isFinishing()) {
+                        // TODO: you may replace this dummy implementation and instead load POI information e.g. from your database
+                        Location poiData = new Location("");
+                        poiData.setLatitude(26.419474);
+                        poiData.setLongitude(-81.810043);
+                        poiData.setAltitude(100);
+                        callJavaScript("World.loadPoisFromJsonData", new String[] { poiData.toString() });
+                    }
+
+                    isLoading = false;
+                }
+            });
+            t.start();
+        }
+    }
+
+    /**
+     * call JacaScript in architectView
+     * @param methodName
+     * @param arguments
+     */
+    private void callJavaScript(final String methodName, final String[] arguments) {
+        final StringBuilder argumentsString = new StringBuilder("");
+        for (int i= 0; i<arguments.length; i++) {
+            argumentsString.append(arguments[i]);
+            if (i<arguments.length-1) {
+                argumentsString.append(", ");
+            }
+        }
+
+        if (this.architectView!=null) {
+            final String js = ( methodName + "( " + argumentsString.toString() + " );" );
+            this.architectView.callJavascript(js);
+        }
+    }
+
+
     abstract String getAssetPath();
+
+    abstract void addPoiToAR(double lat, double lon, double alt);
 }
